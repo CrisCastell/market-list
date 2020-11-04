6958f75eb069733f9f3c7f085c32079680b5f6c1
CrisCastell <cristiamcas2000@gmail.com>
2020-11-03 20:38:59 -0400

    Updates

---
+++ precache-manifest.c80fb22f5d49ce03133383b526e780d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf9cd97fbe988406d325cef10141894",
+    "url": "/market-list/index.html"
+  },
+  {
+    "revision": "d94785c6efd743db3b73",
+    "url": "/market-list/static/css/main.f4220d3e.chunk.css"
+  },
+  {
+    "revision": "202ce957b32f5877825a",
+    "url": "/market-list/static/js/2.bc86bfff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/market-list/static/js/2.bc86bfff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94785c6efd743db3b73",
+    "url": "/market-list/static/js/main.de5d85ce.chunk.js"
+  },
+  {
+    "revision": "fbf0a1194618530f4e2c",
+    "url": "/market-list/static/js/runtime-main.3ae98ec2.js"
+  }
+]);
